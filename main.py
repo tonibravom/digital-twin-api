44ca3dab612b2cf98f4a6c849de0ab4f71a42476
tonibravom <tonibravom@gmail.com>
2026-09-15 10:42:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["GET","PUT"],
     allow_headers=["*"],
 )
 
